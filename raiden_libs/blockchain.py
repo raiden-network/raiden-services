065e538d96352fafc206195dbc26dd867f09a94f
Paul Lange <palango@gmx.de>
2018-03-08 17:22:34 +0100

    Add BlockchainListener
    This is the blockchain monitor from microraiden, but decoupled from
    the channel manager.

---
+++ raiden_libs/blockchain.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+import logging
+import sys
+import requests
+from typing import Callable, Dict, Union, List
+
+from web3 import Web3
+from web3.contract import Contract
+import gevent
+
+
+log = logging.getLogger(__name__)
+
+
+def get_events(
+    w3: Web3,
+    contract: Contract,
+    event_name: str,
+    from_block: Union[int, str] = 0,
+    to_block: Union[int, str] = 'latest',
+) -> List:
+    """
+    Returns events emmitted by a contract for a given event name,
+    within a certain range.
+
+    Args:
+        w3: A Web3 instance
+        contract: A contract
+        event_name: The name of the event(s)
+        from_block: The block to start search events
+        to_block: The block to stop searching for events
+
+    Returns:
+        Matching events
+    """
+
+    filter = contract.eventFilter(event_name, {
+        'fromBlock': from_block,
+        'toBlock': to_block,
+    })
+
+    events = filter.get_all_entries()
+
+    w3.eth.uninstallFilter(filter.filter_id)
+    return events
+
+
+class BlockchainListener(gevent.Greenlet):
+    """ A class listening for events on a given contract. """
+
+    def __init__(
+        self,
+        web3: Web3,
+        contract_address: str,
+        contract_abi: Dict,
+        required_confirmations: int = 4,
+        sync_chunk_size: int = 100_000,
+        poll_interval: int = 2,
+        sync_start_block: int = 0
+    ):
+        """
+        Creates a new BlockchainListener
+        Args:
+            web3: A Web3 instance
+            contract_address: The address of the contract
+            contract_abi: The ABI of the contract
+            required_confirmations: The number of confirmations required to call a block confirmed
+            sync_chunk_size: The size of the chunks used during syncing
+            poll_interval: The interval used between polls
+            sync_start_block: The block number syncing is started at
+        """
+        self.contract = web3.eth.contract(contract_address, abi=contract_abi)
+
+        self.required_confirmations = required_confirmations
+        self.web3 = web3
+
+        self.confirmed_callbacks: Dict[str, Callable] = {}
+        self.unconfirmed_callbacks: Dict[str, Callable] = {}
+
+        self.wait_sync_event = gevent.event.Event()
+        self.is_connected = gevent.event.Event()
+        self.sync_chunk_size = sync_chunk_size
+        self.running = False
+        self.poll_interval = poll_interval
+
+        self.unconfirmed_head_number = sync_start_block
+        self.confirmed_head_number = sync_start_block
+        self.unconfirmed_head_hash = None
+        self.confirmed_head_hash = None
+
+    def add_confirmed_listener(self, event_name: str, callback: Callable):
+        """ Add a callback to listen for confirmed events. """
+        self.confirmed_callbacks[event_name] = callback
+
+    def add_unconfirmed_listener(self, event_name: str, callback: Callable):
+        """ Add a callback to listen for unconfirmed events. """
+        self.unconfirmed_callbacks[event_name] = callback
+
+    def _run(self):
+        self.running = True
+        log.info('Starting blockchain polling (interval %ss)', self.poll_interval)
+        while self.running:
+            try:
+                self._update()
+                self.is_connected.set()
+                if self.wait_sync_event.is_set():
+                    gevent.sleep(self.poll_interval)
+            except requests.exceptions.ConnectionError as e:
+                endpoint = self.web3.currentProvider.endpoint_uri
+                log.warning(
+                    'Ethereum node (%s) refused connection. Retrying in %d seconds.' %
+                    (endpoint, self.poll_interval)
+                )
+                gevent.sleep(self.poll_interval)
+                self.is_connected.clear()
+            log.info('Stopped blockchain polling')
+
+    def stop(self):
+        """ Stops the BlockchainListener. """
+        self.running = False
+
+    def wait_sync(self):
+        """Blocks until event polling is up-to-date with a most recent block of the blockchain. """
+        self.wait_sync_event.wait()
+
+    def _update(self):
+        current_block = self.web3.eth.blockNumber
+
+        # reset unconfirmed channels in case of reorg
+        if self.wait_sync_event.is_set():  # but not on first sync
+            if current_block < self.unconfirmed_head_number:
+                log.info('Chain reorganization detected. '
+                         'Resyncing unconfirmed events (unconfirmed_head=%d) [@%d]' %
+                         (self.unconfirmed_head_number, self.web3.eth.blockNumber))
+                self.cm.reset_unconfirmed()
+            try:
+                # raises if hash doesn't exist (i.e. block has been replaced)
+                self.web3.eth.getBlock(self.unconfirmed_head_hash)
+            except ValueError:
+                log.info('Chain reorganization detected. '
+                         'Resyncing unconfirmed events (unconfirmed_head=%d) [@%d]. '
+                         '(getBlock() raised ValueError)' %
+                         (self.unconfirmed_head_number, current_block))
+                self.unconfirmed_head_number = self.confirmed_head_number
+                self.unconfirmed_head_hash = self.confirmed_head_hash
+
+            # in case of reorg longer than confirmation number fail
+            try:
+                self.web3.eth.getBlock(self.confirmed_head_hash)
+            except ValueError:
+                log.critical('Events considered confirmed have been reorganized')
+                assert False  # unreachable as long as confirmation level is set high enough
+
+        new_unconfirmed_head_number = self.unconfirmed_head_number + self.sync_chunk_size
+        new_unconfirmed_head_number = min(new_unconfirmed_head_number, current_block)
+        new_confirmed_head_number = max(
+            new_unconfirmed_head_number - self.required_confirmations,
+            0
+        )
+
+        # return if blocks have already been processed
+        if (self.confirmed_head_number >= new_confirmed_head_number and
+                self.unconfirmed_head_number >= new_unconfirmed_head_number):
+            return
+
+        # filter for events after block_number
+        filters_confirmed = {
+            'from_block': self.confirmed_head_number + 1,
+            'to_block': new_confirmed_head_number,
+        }
+        filters_unconfirmed = {
+            'from_block': self.unconfirmed_head_number + 1,
+            'to_block': new_unconfirmed_head_number,
+        }
+        log.debug(
+            'Filtering for events u:%s-%s c:%s-%s @%d',
+            filters_unconfirmed['from_block'],
+            filters_unconfirmed['to_block'],
+            filters_confirmed['from_block'],
+            filters_confirmed['to_block'],
+            current_block
+        )
+
+        # filter confirmed events
+        for event_name, callback in self.confirmed_callbacks.items():
+            events = get_events(
+                self.web3,
+                self.contract,
+                event_name,
+                **filters_confirmed
+            )
+            for event in events:
+                log.debug(
+                    'Received confirmed %s event',
+                    event_name,
+                )
+                callback(event)
+
+        # filter unconfirmed events
+        for event_name, callback in self.unconfirmed_callbacks.items():
+            events = get_events(
+                self.web3,
+                self.contract,
+                event_name,
+                **filters_unconfirmed
+            )
+            for event in events:
+                log.debug(
+                    'Received unconfirmed %s event',
+                    event_name,
+                )
+                callback(event)
+
+        # update head hash and number
+        try:
+            new_unconfirmed_head_hash = self.web3.eth.getBlock(new_unconfirmed_head_number).hash
+            new_confirmed_head_hash = self.web3.eth.getBlock(new_confirmed_head_number).hash
+        except AttributeError:
+            log.critical("RPC endpoint didn't return proper info for an existing block "
+                         "(%d,%d)" % (new_unconfirmed_head_number, new_confirmed_head_number))
+            log.critical("It is possible that the blockchain isn't fully synced. "
+                         "This often happens when Parity is run with --fast or --warp sync.")
+            log.critical("Cannot continue - check status of the ethereum node.")
+            sys.exit(1)
+
+        self.unconfirmed_head_number = new_unconfirmed_head_number
+        self.unconfirmed_head_hash = new_unconfirmed_head_hash
+        self.confirmed_head_number = new_confirmed_head_number
+        self.confirmed_head_hash = new_confirmed_head_hash
+
+        if not self.wait_sync_event.is_set() and new_unconfirmed_head_number == current_block:
+            self.wait_sync_event.set()
