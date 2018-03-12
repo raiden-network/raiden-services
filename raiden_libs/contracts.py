460fa35456fe99bfc597454bdf6bedf7ea1c01d2
Paul Lange <palango@gmx.de>
2018-03-12 11:57:38 +0100

    Fix tox config

--- raiden_libs/contracts.py
+++ raiden_libs/contracts.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 import json
 
+
 class ContractManager:
     def __init__(self, abi_path: str):
         with open(abi_path) as json_file:
-            self.abi = json.load(json_file)
+            self.data = json.load(json_file)
 
     def get_contract_abi(self, contract_name: str) -> dict:
         """ Returns the ABI for a given contract. """
-        return self.abi[contract_name]
+        return self.data[contract_name]['abi']
 
     def get_event_abi(self, contract_name: str, event_name: str):
         """ Returns the ABI for a given event. """
         contract_abi = self.get_contract_abi(contract_name)
         result = [
-            x for x in contract_abi['abi']
+            x for x in contract_abi
             if x['type'] == 'event' and x['name'] == event_name
         ]
 
