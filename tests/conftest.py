141bef2e8ff030255d55677b05e0dd69c0e675c0
Paul Lange <palango@gmx.de>
2018-03-28 13:15:17 +0200

    Remove use_tester fixture, ease dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,3 @@ from raiden_libs.test.fixtures.client import *  # noqa
 from raiden_libs.test.fixtures.web3 import *  # noqa
 
 from .fixture_overwrites import *  # noqa
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--no-tester",
-        action="store_false",
-        default=True,
-        dest='use_tester',
-        help="Use a real RPC endpoint instead of the tester chain."
-    )
