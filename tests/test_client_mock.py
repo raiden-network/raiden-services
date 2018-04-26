2995cb48933f936bc9d0c6b19c30c05d9a8c2f92
Jozef Henzl <popcorp86@gmail.com>
2018-04-26 17:10:05 +0200

    UpdateTransfer for mocked client
    - updates updateTransfer method used by the mocked client
    - adds some tests to verify channel life cycle

--- tests/test_client_mock.py
+++ tests/test_client_mock.py
@@ -50,17 +50,49 @@ def test_message_signature(generate_raiden_clients):
     assert is_same_address(fee_info.signer, c1.address)
 
 
-def test_close_settle(generate_raiden_clients, wait_for_blocks):
+def test_close_settle(generate_raiden_clients, wait_for_blocks, standard_token_contract):
+    """Tests channel life cycle for mocked client"""
     c1, c2 = generate_raiden_clients(2)
     c1.open_channel(c2.address)
+
+    initial_balance_c1 = standard_token_contract.functions.balanceOf(c1.address).call()
+    initial_balance_c2 = standard_token_contract.functions.balanceOf(c2.address).call()
+    transfer_c1 = 5
+    transfer_c2 = 6
+
+    c1.deposit_to_channel(c2.address, 100)
+    c2.deposit_to_channel(c1.address, 100)
 
     balance_proof = c2.get_balance_proof(
         c1.address,
         nonce=1,
-        transferred_amount=5,
+        transferred_amount=transfer_c1,
+        locked_amount=0,
+        locksroot='0x%064x' % 0,
+        additional_hash='0x%064x' % 0
+    )
+    balance_proof_c2 = c1.get_balance_proof(
+        c2.address,
+        nonce=2,
+        transferred_amount=transfer_c2,
         locked_amount=0,
         locksroot='0x%064x' % 0,
         additional_hash='0x%064x' % 0
     )
 
     c1.close_channel(c2.address, balance_proof)
+
+    c2.update_transfer(c1.address, balance_proof_c2)
+
+    wait_for_blocks(40)
+    c1.settle_channel(
+        c2.address,
+        (balance_proof_c2.transferred_amount, balance_proof.transferred_amount),
+        (balance_proof_c2.locked_amount, balance_proof.locked_amount),
+        (balance_proof_c2.locksroot, balance_proof.locksroot)
+    )
+
+    final_balance_c1 = standard_token_contract.functions.balanceOf(c1.address).call()
+    final_balance_c2 = standard_token_contract.functions.balanceOf(c2.address).call()
+    assert final_balance_c1 == initial_balance_c1 + (transfer_c1 - transfer_c2)
+    assert final_balance_c2 == initial_balance_c2 - (transfer_c1 - transfer_c2)
