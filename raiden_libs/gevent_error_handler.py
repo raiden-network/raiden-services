f1d80e1847461541b574a08d0c7930a888abd0f5
Augusto F. Hack <hack.augusto@gmail.com>
2018-06-07 12:16:15 -0300

    warn about infinit loops

--- raiden_libs/gevent_error_handler.py
+++ raiden_libs/gevent_error_handler.py
@@ -5,8 +5,10 @@ IGNORE_ERROR = Hub.SYSTEM_ERROR + Hub.NOT_ERROR
 
 
 def register_error_handler(error_handler):
-
     Hub._origin_handle_error = Hub.handle_error
+
+    msg = 'registering the same error handler twice will result in a infinite loop'
+    assert error_handler != Hub._origin_handle_error, msg
 
     def custom_handle_error(self, context, type, value, tb):
         if not issubclass(type, IGNORE_ERROR):
