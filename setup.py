f76d420aec24a20697cc7ca71f894e4d0edf54e0
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2018-08-31 16:24:08 +0200

    Fix setup.py develop with git requirement

--- setup.py
+++ setup.py
@@ -2,26 +2,28 @@
 
 """The setup script."""
 
+import re
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements_replacements = {
-    (
-        'git+https://github.com/matrix-org/'
-        'matrix-python-sdk.git@9ccbaa1#egg=matrix_client'
-    ): 'matrix-client',
-}
 
-requirements = list(set(
-    requirements_replacements.get(requirement.strip(), requirement.strip())
-    for requirement in open('requirements.txt') if not requirement.lstrip().startswith('#')
-))
+def get_egg_or_req(req):
+    match = re.search('#egg=([^#@]+)', req, re.U | re.I)
+    return (match and match.group(1)) or req
 
-setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest', ]
+with open('requirements.txt') as req_file:
+    requirements = list({
+        get_egg_or_req(requirement)
+        for requirement in req_file
+        if requirement.strip() and not requirement.lstrip().startswith('#')
+    })
+
+setup_requirements = ['pytest-runner']
+
+test_requirements = ['pytest']
 
 setup(
     author="Brainbot Labs Est.",
@@ -39,7 +41,7 @@ setup(
     include_package_data=False,
     keywords='raiden_libs',
     name='raiden_libs',
-    packages=find_packages(exclude=['tests', ]),
+    packages=find_packages(exclude=['tests']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
