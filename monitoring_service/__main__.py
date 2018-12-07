dad4fdb7d25acf5426ba4a9f1a74034b14c92244
Karl Bartel <karl42@gmail.com>
2018-12-07 09:50:36 +0100

    Use precompiled contracts instead of source

--- monitoring_service/__main__.py
+++ monitoring_service/__main__.py
@@ -9,7 +9,7 @@ from monitoring_service.state_db import StateDB
 from monitoring_service.api.rest import ServiceApi
 from monitoring_service.blockchain import BlockchainMonitor
 from raiden_libs.no_ssl_patch import no_ssl_verification
-from raiden_contracts.contract_manager import ContractManager, contracts_source_path
+from raiden_contracts.contract_manager import ContractManager, contracts_precompiled_path
 
 
 @click.command()
@@ -88,7 +88,7 @@ def main(
     web3 = Web3(HTTPProvider(eth_rpc))
     blockchain = BlockchainMonitor(web3)
     db = StateDB(state_db)
-    contract_manager = ContractManager(contracts_source_path())
+    contract_manager = ContractManager(contracts_precompiled_path())
 
     monitor = MonitoringService(
         contract_manager,
