14085e00006b18bef7d858a720294933fd06a097
Karl Bartel <karl42@gmail.com>
2018-12-11 16:23:56 +0100

    Add isort config

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+ISORT_PARAMS = --ignore-whitespace --settings-path . --recursive monitoring_service/
+
 all: lint mypy
 
 lint:
 	flake8 monitoring_service/
+	isort $(ISORT_PARAMS) --diff --check-only
 
 mypy:
 	mypy --ignore-missing-imports monitoring_service/
+
+isort:
+	isort $(ISORT_PARAMS)
--- monitoring_service/test/conftest.py
+++ monitoring_service/test/conftest.py
@@ -1,5 +1,11 @@
-from gevent import monkey
-monkey.patch_all()
+"""
+For some reason isort changes the behaviour of this file. Might have something
+to do with monkey.patch_all.
+
+isort:skip_file
+"""
+from gevent import monkey  # isort:skip
+monkey.patch_all()         # isort:skip
 from raiden_libs.test.fixtures import *        # noqa
 from raiden_libs.test.fixtures.web3 import *        # noqa
 from raiden_libs.test.fixtures.address import *     # noqa
