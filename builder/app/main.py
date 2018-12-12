d6b551152112efb488aab38ceb40d1ed131fbdd9
Paul Lange <palango@gmx.de>
2018-12-12 14:03:38 +0100

    Initial commit

---
+++ builder/app/main.py
@@ -0,0 +1,62 @@
+import hashlib
+import os
+import subprocess
+import sys
+from pprint import pprint
+
+import requests
+from flask import Flask, request
+
+
+REPOS = {
+    'raiden-network/raiden-pathfinding-service': {
+        'master': {
+            'source': '/data/raiden-pathfinding-service',
+            'deployment': '/data/services-dev',
+        },
+    },
+}
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['get', 'post'])
+def main():
+    data = request.json
+    repo = data.get('repository', {}).get('full_name', '')
+    branch = data.get('ref', '').replace('refs/heads/', '')
+    branch_config = REPOS.get(repo)
+    pprint(repo)
+    pprint(branch)
+    if branch_config and branch in branch_config:
+        res = build(branch, **branch_config[branch])
+        if res:
+            pprint(
+                {
+                    'repo': repo,
+                    'branch': branch,
+                    'head_commit': data['head_commit'],
+                    'pusher': data['pusher'],
+                    'build_result': res.split(b'\n')
+                },
+                stream=sys.stderr
+            )
+
+        else:
+            print("Error building", file=sys.stderr)
+    return "OK"
+
+
+def build(branch, source, deployment, **kw):
+    try:
+        os.chdir(source)
+        subprocess.check_output(["git", "fetch", "--all"])
+        subprocess.check_output(["git", "reset", "--hard", f"origin/{branch}"])
+
+        os.chdir(deployment)
+        subprocess.check_output(["docker-compose", "build"])
+        subprocess.check_output(["docker-compose", "stop"])
+        subprocess.check_output(["docker-compose", "up", "-d")
+    except:
+        return False
+    return True
