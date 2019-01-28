7a76b41255411dfef02af6b97b0a35fbcbfb4c35
Paul Lange <palango@gmx.de>
2019-01-28 15:57:59 +0100

    Remove old MS code

--- monitoring/old/__init__.py
+++
@@ -1,8 +0,0 @@
-from gevent import monkey
-monkey.patch_all()
-import requests # noqa
-from old.server import MonitoringService
-
-__all__ = [
-    'MonitoringService',
-]
--- monitoring/old/tasks/__init__.py
+++
@@ -1,7 +0,0 @@
-from .on_channel_close import OnChannelClose            # noqa
-from .on_channel_settle import OnChannelSettle          # noqa
-
-__all__ = [
-    'OnChannelClose',
-    'OnChannelSettle',
-]
