94c111f6ee4c8e3595e9ad7974aff8c41bc7789c
Paul Lange <palango@gmx.de>
2019-02-01 11:14:05 +0100

    Remove old reference to raiden-libs

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ from setuptools import find_packages, setup
 REQ_REPLACE = {
     'git+https://github.com/matrix-org/matrix-python-sdk.git': 'matrix-client',
     'git+https://github.com/raiden-network/raiden-contracts.git': 'raiden-contracts',
-    'git+https://github.com/raiden-network/raiden-libs.git': 'raiden-libs',
 }
 
 DESCRIPTION = 'Raiden Services contain additional tools for the Raiden Network.'
