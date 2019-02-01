63691b2e5701d3ef2452fa08037d320d27d51a1a
Karl Bartel <karl42@gmail.com>
2019-02-01 14:54:37 +0100

    Simply monitoring service fixture
    by reusing the raiden_service_bundle fixture from raiden-contracts.

--- tests/monitoring/fixtures/server.py
+++ tests/monitoring/fixtures/server.py
@@ -2,15 +2,10 @@ import logging
 
 import gevent.event  # noqa  needed for DummyTransport fixture
 import pytest
-from eth_utils import is_checksum_address, to_checksum_address
 from request_collector.server import RequestCollector
-from web3 import Web3
-from web3.utils.transactions import wait_for_transaction_receipt
 
 from monitoring_service.service import MonitoringService
-from raiden_contracts.constants import CONTRACT_MONITORING_SERVICE, CONTRACT_RAIDEN_SERVICE_BUNDLE
 from raiden_contracts.contract_manager import ContractManager
-from raiden_libs.private_contract import PrivateContract
 from raiden_libs.test.mocks.dummy_transport import DummyTransport
 from raiden_libs.utils import private_key_to_address
 
@@ -26,48 +21,6 @@ def server_private_key(get_random_privkey, ethereum_tester):
     return key
 
 
-def register_service(
-    web3: Web3,
-    contract_manager: ContractManager,
-    msc_contract_address: str,
-    private_key: str,
-    deposit: int = 10,  # any amount works now
-):
-    """Register service with a Monitor service contract"""
-    service_address = private_key_to_address(private_key)
-    assert is_checksum_address(msc_contract_address)
-    assert is_checksum_address(service_address)
-    monitor_contract_abi = contract_manager.get_contract_abi(CONTRACT_MONITORING_SERVICE)
-    monitor_contract = PrivateContract(web3.eth.contract(
-        abi=monitor_contract_abi,
-        address=msc_contract_address,
-    ))
-    bundle_contract_abi = contract_manager.get_contract_abi(CONTRACT_RAIDEN_SERVICE_BUNDLE)
-    raiden_service_bundle_address = to_checksum_address(monitor_contract.functions.rsb().call())
-    bundle_contract = PrivateContract(web3.eth.contract(
-        abi=bundle_contract_abi,
-        address=raiden_service_bundle_address,
-    ))
-
-    # approve funds for MSC
-    token_address = to_checksum_address(monitor_contract.functions.token().call())
-    token_abi = contract_manager.get_contract_abi('Token')
-    token_contract = web3.eth.contract(abi=token_abi, address=token_address)
-    token_contract = PrivateContract(token_contract)
-    tx = token_contract.functions.approve(raiden_service_bundle_address, deposit).transact(
-        private_key=private_key,
-    )
-    wait_for_transaction_receipt(web3, tx)
-
-    # register MS
-    tx = bundle_contract.functions.deposit(deposit).transact(
-        private_key=private_key,
-    )
-    # check if MS is really registered
-    wait_for_transaction_receipt(web3, tx)
-    return bundle_contract.functions.deposits(service_address).call() > 0
-
-
 @pytest.fixture
 def dummy_transport():
     return DummyTransport()
@@ -81,15 +34,19 @@ def monitoring_service(
     token_network_registry_contract,
     send_funds,
     contracts_manager: ContractManager,
+    raiden_service_bundle,
+    custom_token,
 ):
-    # send some eth & tokens to MS
-    send_funds(private_key_to_address(server_private_key))
-    register_service(
-        web3=web3,
-        contract_manager=contracts_manager,
-        msc_contract_address=monitoring_service_contract.address,
-        private_key=server_private_key,
-    )
+    # register MS in ServiceRegistry
+    ms_address = private_key_to_address(server_private_key)
+    send_funds(ms_address)
+    deposit = 10  # any amount is sufficient for regsitration, right now
+    custom_token.functions.mint(deposit).transact({'from': ms_address})
+    custom_token.functions.approve(
+        raiden_service_bundle.address,
+        deposit,
+    ).transact({'from': ms_address})
+    raiden_service_bundle.functions.deposit(deposit).transact({'from': ms_address})
 
     ms = MonitoringService(
         web3=web3,
