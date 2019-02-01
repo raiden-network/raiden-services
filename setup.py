70df80b46ee6c7d1668467ab9fd0eb89a186f986
Paul Lange <palango@gmx.de>
2019-02-01 15:44:55 +0100

    Don't generate universal wheels

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def read(*names, **kwargs):
 
 setup(
     name='raiden-services',
-    version='0.1.0',
+    version='0.0.1',
     license='MIT',
     description=DESCRIPTION,
     author='Brainbot Labs Est.',
