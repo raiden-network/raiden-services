1b9eead6e98095ae8bb3b3cea42134d5a6a92efa
Paul Lange <palango@gmx.de>
2019-02-04 15:25:07 +0100

    Add tests for monitor_new_balance_proof_event_handler

--- tests/monitoring/monitoring_service/test_handlers.py
+++ tests/monitoring/monitoring_service/test_handlers.py
@@ -3,12 +3,19 @@ from unittest.mock import Mock
 import pytest
 
 from monitoring_service.database import Database
-from monitoring_service.events import Event, ReceiveChannelClosedEvent, ReceiveChannelOpenedEvent, ReceiveNonClosingBalanceProofUpdatedEvent
+from monitoring_service.events import (
+    Event,
+    ReceiveChannelClosedEvent,
+    ReceiveChannelOpenedEvent,
+    ReceiveNonClosingBalanceProofUpdatedEvent,
+    ReceiveMonitoringNewBalanceProofEvent,
+)
 from monitoring_service.handlers import (
     Context,
     channel_closed_event_handler,
     channel_opened_event_handler,
     channel_non_closing_balance_proof_updated_event_handler,
+    monitor_new_balance_proof_event_handler,
 )
 from monitoring_service.states import BlockchainState, MonitoringServiceState
 from raiden_contracts.constants import ChannelState
@@ -212,3 +219,84 @@ def test_channel_bp_updated_event_handler_sets_update_status_if_not_set(
     assert channel.update_status is not None
     assert channel.update_status.nonce == 5
     assert channel.update_status.update_sender_address == 'A'
+
+
+def test_monitor_new_balance_proof_event_handler_ignores_other_event(
+    context: Context,
+):
+    event = Event()
+
+    with pytest.raises(AssertionError):
+        monitor_new_balance_proof_event_handler(
+            event=event,
+            context=context,
+        )
+
+
+def test_monitor_new_balance_proof_event_handler_sets_update_status_if_not_set(
+    context: Context,
+):
+    event = ReceiveChannelOpenedEvent(
+        token_network_address='abc',
+        channel_identifier=3,
+        participant1='A',
+        participant2='B',
+        settle_timeout=100,
+        block_number=42,
+    )
+
+    assert len(context.db.channels) == 0
+    channel_opened_event_handler(event, context)
+
+    assert len(context.db.channels) == 1
+    assert context.db.channels[0].state == ChannelState.OPENED
+
+    event2 = ReceiveChannelClosedEvent(
+        token_network_address='abc',
+        channel_identifier=3,
+        closing_participant='B',
+        block_number=52,
+    )
+    channel_closed_event_handler(event2, context)
+
+    assert len(context.db.channels) == 1
+    assert context.db.channels[0].state == ChannelState.CLOSED
+
+    event3 = ReceiveMonitoringNewBalanceProofEvent(
+        token_network_address='abc',
+        channel_identifier=3,
+        reward_amount=1,
+        nonce=2,
+        ms_address='C',
+        raiden_node_address='B',
+        block_number=23,
+    )
+
+    channel = context.db.get_channel(event3.token_network_address, event3.channel_identifier)
+    assert channel.update_status is None
+
+    monitor_new_balance_proof_event_handler(event3, context)
+
+    assert len(context.db.channels) == 1
+    channel = context.db.get_channel(event3.token_network_address, event3.channel_identifier)
+    assert channel.update_status is not None
+    assert channel.update_status.nonce == 2
+    assert channel.update_status.update_sender_address == 'C'
+
+    event4 = ReceiveMonitoringNewBalanceProofEvent(
+        token_network_address='abc',
+        channel_identifier=3,
+        reward_amount=1,
+        nonce=5,
+        ms_address='D',
+        raiden_node_address='B',
+        block_number=23,
+    )
+
+    monitor_new_balance_proof_event_handler(event4, context)
+
+    assert len(context.db.channels) == 1
+    channel = context.db.get_channel(event3.token_network_address, event3.channel_identifier)
+    assert channel.update_status is not None
+    assert channel.update_status.nonce == 5
+    assert channel.update_status.update_sender_address == 'D'
