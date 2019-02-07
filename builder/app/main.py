43946983eb270eb0157b7a003fa9150d65b891b1
Paul Lange <palango@gmx.de>
2019-02-07 15:42:52 +0100

    Add test PFS instances for other testnets

--- builder/app/main.py
+++ builder/app/main.py
@@ -12,7 +12,7 @@ REPOS = {
             'source': '/data/raiden-services',
             'deployment': '/data/services-dev',
         },
-        'name': 'pfs-ropsten',
+        'names': ['pfs-ropsten', 'pfs-rinkeby', 'pfs-kovan'],
     },
 }
 
@@ -26,7 +26,7 @@ def main():
     branch = data.get('ref', '').replace('refs/heads/', '')
     branch_config = REPOS.get(repo)
     if branch_config and branch in branch_config:
-        res = build(branch, branch_config['name'], **branch_config[branch])
+        res = build(branch, branch_config['names'], **branch_config[branch])
         if res:
             pprint(
                 {
@@ -47,23 +47,24 @@ def _print(s):
     print(s, file=sys.stderr)
 
 
-def build(branch, container_name, source, deployment, **kw):
+def build(branch, container_names, source, deployment, **kw):
     try:
         _print(f'Switching to {source}')
-        _print(f'Container name = {container_name}')
+        _print(f'Container names = {container_names}')
         os.chdir(source)
-        _print(f'git fetch')
+        _print('git fetch')
         subprocess.check_output(["git", "fetch", "--all"])
-        _print(f'git reset')
+        _print('git reset')
         subprocess.check_output(["git", "reset", "--hard", f"origin/{branch}"])
 
         _print(f'Switching to {deployment}')
         os.chdir(deployment)
-        _print(f'docker build')
+        _print('docker build')
         subprocess.check_output(["docker-compose", "build"])
-        _print(f'docker down')
-        subprocess.check_output(["docker-compose", "stop", container_name])
-        _print(f'docker up')
+        for name in container_names:
+            _print(f'docker down: {name}')
+            subprocess.check_output(["docker-compose", "stop", name])
+        _print('docker up')
         subprocess.check_output(["docker-compose", "up", "-d"])
     except Exception as e:
         _print(str(e))
