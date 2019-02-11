34c87fe61d5670b003de7648c27922b5e26b72ce
Paul Lange <palango@gmx.de>
2019-02-11 17:20:17 +0100

    Add raiden dependency
    This also forces us to downgrade some dependencies, see
    https://github.com/raiden-network/raiden/issues/3460 for more info.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools import find_packages, setup
 REQ_REPLACE = {
     'git+https://github.com/matrix-org/matrix-python-sdk.git': 'matrix-client',
     'git+https://github.com/raiden-network/raiden-contracts.git': 'raiden-contracts',
+    'git+https://github.com/palango/raiden.git@services-integration': 'raiden',
 }
 
 DESCRIPTION = 'Raiden Services contain additional tools for the Raiden Network.'
