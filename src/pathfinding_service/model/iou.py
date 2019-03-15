d2be839cf663dd8f9c59acc2feb974eff5b7314b
Karl Bartel <karl42@gmail.com>
2019-03-15 10:25:46 +0100

    Use properly typed dataclass
    The same dataclass is used for mypy type checking and for marshmallow
    (de)serialization.
    
    The signature is now stored as bytes in the dataclass, which is
    consistent with raiden.

--- src/pathfinding_service/model/iou.py
+++ src/pathfinding_service/model/iou.py
@@ -1,54 +1,36 @@
 from typing import ClassVar, Optional, Type
 
 import marshmallow
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from eth_abi import encode_single
-from eth_utils import decode_hex
+from eth_utils import decode_hex, encode_hex
 from marshmallow_dataclass import add_schema
 from web3 import Web3
 
+from raiden.utils.typing import BlockNumber, Signature, TokenAmount
 from raiden_libs.exceptions import InvalidSignature
+from raiden_libs.types import Address
 from raiden_libs.utils import eth_recover
 
-# from raiden_libs.types import Address
-# from raiden.utils.typing import (
-#     BlockNumber,
-#     Signature,
-#     TokenAmount,
-# )
 
+class HexedBytes(marshmallow.fields.Field):
+    """ Use `bytes` in the dataclass, serialize to hex encoding"""
 
-# @dataclass
-# class IOU:
-#     sender: Address
-#     receiver: Address
-#     amount: TokenAmount
-#     expiration_block: BlockNumber
-#     signature: Signature
-#     claimed: bool
-#     Schema: ClassVar[Type[marshmallow.Schema]]
+    def _serialize(self, value, attr, obj):
+        return encode_hex(value)
 
-#     def is_signature_valid(self):
-#         packed_data = (
-#             Web3.toBytes(hexstr=self.sender) +
-#             Web3.toBytes(hexstr=self.receiver) +
-#             encode_single('uint256', self.amount) +
-#             encode_single('uint256', self.expiration_block)
-#         )
-#         try:
-#             return eth_recover(packed_data, decode_hex(self.signature)) == self.sender
-#         except InvalidSignature:
-#             return False
+    def _deserialize(self, value, attr, data):
+        return decode_hex(value)
 
 
 @add_schema
 @dataclass
 class IOU:
-    sender: str
-    receiver: str
-    amount: int
-    expiration_block: int
-    signature: str
+    sender: Address
+    receiver: Address
+    amount: TokenAmount
+    expiration_block: BlockNumber
+    signature: Signature = field(metadata={"marshmallow_field": HexedBytes()})
     claimed: Optional[bool] = None
     Schema: ClassVar[Type[marshmallow.Schema]]
 
@@ -60,6 +42,6 @@ class IOU:
             encode_single('uint256', self.expiration_block)
         )
         try:
-            return eth_recover(packed_data, decode_hex(self.signature)) == self.sender
+            return eth_recover(packed_data, self.signature) == self.sender
         except InvalidSignature:
             return False
