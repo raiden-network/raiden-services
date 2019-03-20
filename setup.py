744ee8cb84e4a31f151092a494e34a37b06b3183
Paul Lange <palango@gmx.de>
2019-03-20 11:59:39 +0100

    Update to contracts 0.15.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 REQ_REPLACE = {
     'git+https://github.com/matrix-org/matrix-python-sdk.git': 'matrix-client',
     # 'git+https://github.com/raiden-network/raiden-contracts.git': 'raiden-contracts',
-    'git+https://github.com/raiden-network/raiden.git': 'raiden',
+    'git+https://github.com/raiden-network/raiden.git@f7fdcf786dbe270523bec214e43c825cc520889e': 'raiden',
 }
 
 DESCRIPTION = 'Raiden Services contain additional tools for the Raiden Network.'
