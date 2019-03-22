96b523850c04038836364ab70cdfb6e24e505896
Karl Bartel <karl42@gmail.com>
2019-03-22 10:40:32 +0100

    Disable gevent `thread` resolver for PFS
    It was causing `socket.gaierror: [Errno -2] Name or service not known`
    errors when run inside docker.

--- src/pathfinding_service/cli.py
+++ src/pathfinding_service/cli.py
@@ -1,5 +1,7 @@
 """Console script for pathfinding_service."""
-from gevent import monkey  # isort:skip # noqa
+from gevent import monkey, config  # isort:skip # noqa
+# there were some issues with the 'thread' resolver, remove it from the options
+config.resolver = ['dnspython', 'ares', 'block'] # noqa
 monkey.patch_all()  # isort:skip # noqa
 
 import json
