80c65674fffc66036356b1bfc527bd848f8c3c90
Karl Bartel <karl42@gmail.com>
2019-03-22 15:40:35 +0100

    Create shared `conftest.py` for PFS/MS/libs

--- tests/libs/conftest.py
+++ tests/libs/conftest.py
@@ -1,10 +1,6 @@
-from gevent import monkey
-
 from raiden_contracts.tests.fixtures import *  # noqa
 from raiden_libs.test.fixtures.address import *  # noqa
 from raiden_libs.test.fixtures.client import *  # noqa
 from raiden_libs.test.fixtures.web3 import *  # noqa
 
 from .fixture_overwrites import *  # noqa
-
-monkey.patch_all()
