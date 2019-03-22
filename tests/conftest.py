103b6904fd3a3289f96db9cc389bea64a1b57a4e
Karl Bartel <karl42@gmail.com>
2019-03-22 10:40:32 +0100

    Disable gevent `thread` resolver for PFS
    It was causing `socket.gaierror: [Errno -2] Name or service not known`
    errors when run inside docker.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
-from gevent import monkey  # isort:skip # noqa
+from gevent import monkey, config  # isort:skip # noqa
+# there were some issues with the 'thread' resolver, remove it from the options
+config.resolver = ['dnspython', 'ares', 'block'] # noqa
 monkey.patch_all()  # isort:skip # noqa
 
 import gc
