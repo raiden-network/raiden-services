6b3907dfd4b5937d00882f94cf2b8ce99de2e3e4
Karl Bartel <karl42@gmail.com>
2019-04-08 16:58:19 +0200

    Calculate start_block only on used contracts

--- src/raiden_libs/contract_info.py
+++ src/raiden_libs/contract_info.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import structlog
 
@@ -15,28 +15,22 @@ log = structlog.get_logger(__name__)
 START_BLOCK_ID = 'block'
 
 
-def get_deployment_infos(chain_id: ChainID, contracts_version: str = None) -> Dict[str, Any]:
+def get_deployment_infos(
+    chain_id: ChainID, contracts: List[str], contracts_version: str = None
+) -> Dict[str, Any]:
     try:
         contract_data = get_contracts_deployment_info(chain_id=chain_id, version=contracts_version)
-        token_network_registry_info = contract_data['contracts'][CONTRACT_TOKEN_NETWORK_REGISTRY]
-        monitor_contract_info = contract_data['contracts'][CONTRACT_MONITORING_SERVICE]
-        user_deposit_contract_info = contract_data['contracts'][CONTRACT_USER_DEPOSIT]
 
-        contracts_start_block = max(
-            0,
-            min(
-                token_network_registry_info['block_number'],
-                monitor_contract_info['block_number'],
-                user_deposit_contract_info['block_number'],
-            ),
-        )
-        return {
-            CONTRACT_TOKEN_NETWORK_REGISTRY: token_network_registry_info['address'],
-            CONTRACT_MONITORING_SERVICE: monitor_contract_info['address'],
-            CONTRACT_USER_DEPOSIT: user_deposit_contract_info['address'],
-            START_BLOCK_ID: contracts_start_block,
+        deployment_info: Dict[str, Address] = {
+            c: contract_data['contracts'][c]['address'] for c in contracts
         }
+        contracts_start_block = max(
+            0, min(contract_data['contracts'][c]['block_number'] for c in contracts)
+        )
+        deployment_info[START_BLOCK_ID] = contracts_start_block
+        return deployment_info
     except ValueError:
+        # TODO
         log.info('No deployed contracts were found at the default registry')
 
         return {
@@ -49,11 +43,9 @@ def get_deployment_infos(chain_id: ChainID, contracts_version: str = None) -> Di
 
 def get_contract_addresses_and_start_block(
     chain_id: ChainID,
+    contracts: List[str],
+    address_overwrites: Dict[str, Address],
     contracts_version: str = None,
-    token_network_registry_address: Address = None,
-    monitor_contract_address: Address = None,
-    user_deposit_contract_address: Address = None,
-    start_block: BlockNumber = BlockNumber(0),
 ) -> Optional[Dict[str, Any]]:
     """ Returns contract addresses and start query block for a given chain and contracts version.
 
@@ -73,26 +65,14 @@ def get_contract_addresses_and_start_block(
         or `None` if the contracts aren't deployed for the given configurations and not all
         options have been supplied.
     """
-    data = get_deployment_infos(chain_id, contracts_version)
+    data = get_deployment_infos(chain_id, contracts, contracts_version)
 
     # overwrite defaults with user settings
-    if token_network_registry_address:
-        data[CONTRACT_TOKEN_NETWORK_REGISTRY] = token_network_registry_address
-    if monitor_contract_address:
-        data[CONTRACT_MONITORING_SERVICE] = monitor_contract_address
-    if user_deposit_contract_address:
-        data[CONTRACT_USER_DEPOSIT] = user_deposit_contract_address
+    for contract, address in address_overwrites.items():
+        data[contract] = address
 
-    # Overwrite start block when all contracts have been overwritten
-    all_addresses_given = all(
-        [token_network_registry_address, monitor_contract_address, user_deposit_contract_address]
-    )
-    if all_addresses_given:
-        data[START_BLOCK_ID] = start_block
+    # Set start block to zero if any contract addresses are overwritten
+    if any(address_overwrites.values()):
+        data[START_BLOCK_ID] = BlockNumber(0)
 
-    # Return infos when all contracts are set, otherwise `None`
-    all_addresses_set = all(v is not None for v in data.values())
-    if all_addresses_set:
-        return data
-    else:
-        return None
+    return data
