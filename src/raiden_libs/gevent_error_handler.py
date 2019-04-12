cd0da7e9250f3126220424a3c5f108a67bc4c5b8
Karl Bartel <karl42@gmail.com>
2019-04-12 11:43:30 +0200

    Simplify gevent error handler
    The error handler allowed nesting of multiple handlers, which often went
    wrong for me causing `RecursionError`s. This in turn obscured the real
    error which made debugging tests unnecessarily hard.
    
    Since we don't actually need to nest error handlers, I simplified the
    handling code which solves the debugging problems for me.

--- src/raiden_libs/gevent_error_handler.py
+++ src/raiden_libs/gevent_error_handler.py
@@ -4,22 +4,19 @@ from gevent.hub import Hub
 
 IGNORE_ERROR = Hub.SYSTEM_ERROR + Hub.NOT_ERROR
 
+_original_error_handler = Hub.handle_error
+
 
 def register_error_handler(error_handler: Callable) -> None:
-    Hub._origin_handle_error = Hub.handle_error
-
-    msg = 'registering the same error handler twice will result in a infinite loop'
-    assert error_handler != Hub._origin_handle_error, msg
+    """Sets the current error handler, overwriting the previous ones"""
 
     def custom_handle_error(self: Any, context: Any, type: Any, value: Any, tb: Any) -> None:
         if not issubclass(type, IGNORE_ERROR):
             error_handler(context, (type, value, tb))
 
-        self._origin_handle_error(context, type, value, tb)
-
     Hub.handle_error = custom_handle_error
 
 
 def unregister_error_handler() -> None:
-    if hasattr(Hub, '_origin_handle_error'):
-        Hub.handle_error = Hub._origin_handle_error
+    """Resets the error handler to the original gevent handler"""
+    Hub.handle_error = _original_error_handler
