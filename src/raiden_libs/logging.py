a4e97aa809204e577ebf1f57c9f01a36ae359ae3
Paul Lange <palango@gmx.de>
2019-04-17 17:45:21 +0200

    Add some logging to Flask requests

--- src/raiden_libs/logging.py
+++ src/raiden_libs/logging.py
@@ -11,20 +11,17 @@ def setup_logging(log_level: str) -> None:
 
     logging.getLogger("web3").setLevel("INFO")
     logging.getLogger("urllib3").setLevel("INFO")
-    # don't show urllib3.connectionpoool errors
-    logging.getLogger("urllib3.connectionpool").setLevel("ERROR")
 
-    chain = [
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.PositionalArgumentsFormatter(),
-        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S.%f"),
-        structlog.processors.StackInfoRenderer(),
-        structlog.processors.format_exc_info,
-        structlog.dev.ConsoleRenderer(),
-    ]
-    structlog.configure_once(
-        processors=chain,
+    structlog.configure(
+        processors=[
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S.%f"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.dev.ConsoleRenderer(),
+        ],
         context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
