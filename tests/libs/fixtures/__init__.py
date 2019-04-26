7be2180ffc3641a0ca49f6043b9ec8d0c849c272
Karl Bartel <karl42@gmail.com>
2019-04-26 14:52:11 +0200

    Use `web3` fixture from raiden-contracts

--- tests/libs/fixtures/__init__.py
+++ tests/libs/fixtures/__init__.py
@@ -1,2 +1,12 @@
-from .cli import *  # noqa
-from .web3 import *  # type: ignore # noqa
+from .cli import default_cli_args
+from .web3 import contracts_manager, keystore_file, mockchain, wait_for_blocks
+
+# Without declaring `__all__`, the names `cli` and `web3` would get imported
+# when doing `from libs.fixtures import *`, which is not intended.
+__all__ = [
+    "wait_for_blocks",
+    "contracts_manager",
+    "keystore_file",
+    "mockchain",
+    "default_cli_args",
+]
--- tests/libs/fixtures/web3.py
+++ tests/libs/fixtures/web3.py
@@ -6,41 +6,11 @@ import gevent
 import pytest
 from eth_account import Account
 from tests.constants import KEYSTORE_FILE_NAME, KEYSTORE_PASSWORD
-from web3 import Web3
-from web3.providers.eth_tester import EthereumTesterProvider
 
 from raiden_contracts.contract_manager import ContractManager, contracts_precompiled_path
-from raiden_contracts.tests.utils.constants import (
-    FAUCET_ADDRESS,
-    FAUCET_ALLOWANCE,
-    FAUCET_PRIVATE_KEY,
-)
 from raiden_libs.events import Event
 
 log = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope="session")
-def web3(patch_genesis_gas_limit, ethereum_tester):
-    """Returns an initialized Web3 instance"""
-    provider = EthereumTesterProvider(ethereum_tester)
-    web3 = Web3(provider)
-    web3.eth.estimateGas = lambda txn: 5_500_000
-
-    # add faucet account to tester
-    ethereum_tester.add_account(FAUCET_PRIVATE_KEY)
-
-    # make faucet rich
-    ethereum_tester.send_transaction(
-        {
-            "from": ethereum_tester.get_accounts()[0],
-            "to": FAUCET_ADDRESS,
-            "gas": 21000,
-            "value": FAUCET_ALLOWANCE,
-        }
-    )
-
-    yield web3
 
 
 @pytest.fixture(scope="session")
