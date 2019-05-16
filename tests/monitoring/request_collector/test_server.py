6d8cdb29b497d2977ffc9bed2fd2c76cad4d2ca7
Karl Bartel <karl42@gmail.com>
2019-05-16 10:46:31 +0200

    Update to latest raiden revision

--- tests/monitoring/request_collector/test_server.py
+++ tests/monitoring/request_collector/test_server.py
@@ -1,32 +1,36 @@
 # pylint: disable=redefined-outer-name
 import pytest
-from eth_utils import decode_hex, to_checksum_address
+from eth_utils import encode_hex, to_checksum_address
 
-from raiden.messages import RequestMonitoring, SignedBlindedBalanceProof
-from raiden.tests.utils import factories
-from raiden.utils.signer import LocalSigner
-from raiden.utils.typing import TokenAmount
+from monitoring_service.states import HashedBalanceProof
+from raiden.messages import RequestMonitoring
+from raiden.utils.typing import ChannelID, TokenAmount, TokenNetworkAddress
 from raiden_contracts.tests.utils import get_random_privkey
+from raiden_libs.utils import private_key_to_address
 
 
 @pytest.fixture
 def build_request_monitoring():
-    non_closing_signer = LocalSigner(decode_hex(get_random_privkey()))
+    non_closing_privkey = get_random_privkey()
+    non_closing_address = private_key_to_address(non_closing_privkey)
 
-    def f(chain_id=1, **kwargs):
-        balance_proof = factories.create(factories.BalanceProofSignedStateProperties(**kwargs))
-        balance_proof.canonical_identifier.chain_identifier = chain_id
-        partner_signed_balance_proof = SignedBlindedBalanceProof.from_balance_proof_signed_state(
-            balance_proof
+    def f(chain_id=1, amount=50, nonce=1):
+        balance_proof = HashedBalanceProof(
+            channel_identifier=ChannelID(1),
+            token_network_address=TokenNetworkAddress(b"1" * 20),
+            chain_id=chain_id,
+            nonce=nonce,
+            additional_hash="",
+            balance_hash=encode_hex(bytes([amount])),
+            priv_key=get_random_privkey(),
         )
-        request_monitoring = RequestMonitoring(
-            onchain_balance_proof=partner_signed_balance_proof, reward_amount=TokenAmount(55)
+        request_monitoring = balance_proof.get_request_monitoring(
+            privkey=non_closing_privkey, reward_amount=TokenAmount(55)
         )
-        request_monitoring.sign(non_closing_signer)
 
         # usually not a property of RequestMonitoring, but added for convenience in these tests
         request_monitoring.non_closing_signer = to_checksum_address(  # type: ignore
-            non_closing_signer.address
+            non_closing_address
         )
         return request_monitoring
 
@@ -55,7 +59,7 @@ def test_invalid_request(ms_database, build_request_monitoring, request_collecto
 
 def test_ignore_old_nonce(ms_database, build_request_monitoring, request_collector):
     def stored_mr_after_proccessing(amount, nonce):
-        request_monitoring = build_request_monitoring(transferred_amount=amount, nonce=nonce)
+        request_monitoring = build_request_monitoring(amount=amount, nonce=nonce)
         request_collector.on_monitor_request(request_monitoring)
         return ms_database.get_monitor_request(
             token_network_address=to_checksum_address(
