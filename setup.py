bd708d90cc3e1044013755d552316c143bb68412
Karl Bartel <karl42@gmail.com>
2019-05-16 10:46:31 +0200

    Update to latest raiden revision

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ from os.path import basename, dirname, join, splitext
 from setuptools import find_packages, setup
 
 REQ_REPLACE = {
-    'git+https://github.com/raiden-network/raiden.git@0f3b278ae2f20b05a692c94f77d17e365926e9b8': 'raiden',
+    "git+https://github.com/raiden-network/raiden.git@aab0172eaa2306e8aa6dc9f540eaf7b9c654f85f": "raiden"
 }
 
-DESCRIPTION = 'Raiden Services contain additional tools for the Raiden Network.'
+DESCRIPTION = "Raiden Services contain additional tools for the Raiden Network."
 
 
 def read_requirements(path: str):
@@ -20,7 +20,7 @@ def read_requirements(path: str):
     with open(path) as requirements:
         for line in requirements.readlines():
             line = line.strip()
-            if line and line[0] in ('#', '-'):
+            if line and line[0] in ("#", "-"):
                 continue
             if line in REQ_REPLACE.keys():
                 line = REQ_REPLACE[line]
@@ -30,49 +30,40 @@ def read_requirements(path: str):
 
 
 def read(*names, **kwargs):
-    return io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8'),
-    ).read()
+    return io.open(join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")).read()
 
 
 setup(
-    name='raiden-services',
-    version='0.0.1',
-    license='MIT',
+    name="raiden-services",
+    version="0.0.1",
+    license="MIT",
     description=DESCRIPTION,
-    author='Brainbot Labs Est.',
-    author_email='contact@brainbot.li',
-    url='https://github.com/raiden-network/raiden-services',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Brainbot Labs Est.",
+    author_email="contact@brainbot.li",
+    url="https://github.com/raiden-network/raiden-services",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords=[
-        'raiden', 'ethereum', 'blockchain',
-    ],
-    install_requires=[
-        read_requirements('requirements.txt'),
-    ],
-    extras_require={
-        'dev': read_requirements('requirements-dev.txt'),
-    },
+    keywords=["raiden", "ethereum", "blockchain"],
+    install_requires=[read_requirements("requirements.txt")],
+    extras_require={"dev": read_requirements("requirements-dev.txt")},
     entry_points={
-        'console_scripts': [
-            'pathfinding-service=pathfinding_service.cli:main',
-            'claim-pfs-fees=pathfinding_service.claim_fees:main',
-            'monitoring-service=monitoring_service.cli:main',
-            'request-collector=request_collector.cli:main',
-        ],
+        "console_scripts": [
+            "pathfinding-service=pathfinding_service.cli:main",
+            "claim-pfs-fees=pathfinding_service.claim_fees:main",
+            "monitoring-service=monitoring_service.cli:main",
+            "request-collector=request_collector.cli:main",
+        ]
     },
 )
