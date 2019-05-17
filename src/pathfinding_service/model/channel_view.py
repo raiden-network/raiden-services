28c803fdfbbcdcdc71c5efc83fa8aec713dfb438
Karl Bartel <karl42@gmail.com>
2019-05-17 15:48:54 +0200

    Prepare data structure for flexible mediation fees

--- src/pathfinding_service/model/channel_view.py
+++ src/pathfinding_service/model/channel_view.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import ClassVar, Type
+from typing import ClassVar, List, Optional, Type
 
 import marshmallow
 from marshmallow_dataclass import add_schema
@@ -14,6 +14,26 @@ from raiden.utils.typing import (
     TokenNetworkAddress,
 )
 from raiden_libs.marshmallow import ChecksumAddress
+
+
+@dataclass
+class FeeSchedule:
+    flat: FeeAmount
+    proportional: float
+    imbalance_penalty: Optional[List[List[FeeAmount]]] = None
+
+    def reversed(self) -> "FeeSchedule":
+        # pylint: disable=not-an-iterable
+        if not self.imbalance_penalty:
+            return self
+        max_x = max(x for x, penalty in self.imbalance_penalty)
+        return FeeSchedule(
+            flat=self.flat,
+            proportional=self.proportional,
+            imbalance_penalty=[
+                [FeeAmount(max_x - x), penalty] for x, penalty in self.imbalance_penalty
+            ],
+        )
 
 
 @add_schema
@@ -34,8 +54,9 @@ class ChannelView:
     reveal_timeout: int = DEFAULT_REVEAL_TIMEOUT
     deposit: TokenAmount = TokenAmount(0)
     update_nonce: Nonce = Nonce(0)
-    absolute_fee: FeeAmount = FeeAmount(0)
-    relative_fee: float = 0
+    fee_schedule: FeeSchedule = field(
+        default_factory=lambda: FeeSchedule(flat=FeeAmount(0), proportional=FeeAmount(0))
+    )
     Schema: ClassVar[Type[marshmallow.Schema]]
 
     def __post_init__(self) -> None:
@@ -48,21 +69,16 @@ class ChannelView:
             self.deposit = TokenAmount(total_deposit)
 
     def update_capacity(
-        self,
-        capacity: TokenAmount,
-        nonce: Nonce = Nonce(0),
-        reveal_timeout: int = None,
-        mediation_fee: FeeAmount = FeeAmount(0),
+        self, capacity: TokenAmount, nonce: Nonce = Nonce(0), reveal_timeout: int = None
     ) -> None:
         self.update_nonce = nonce
         self.capacity = capacity
         if reveal_timeout is not None:
             self.reveal_timeout = reveal_timeout
-        self.absolute_fee = mediation_fee
 
     def fee(self, amount: TokenAmount) -> int:
         """Return the mediation fee for this channel when transferring the given amount"""
-        return int(self.absolute_fee + amount * self.relative_fee)
+        return int(self.fee_schedule.flat + amount * self.fee_schedule.proportional)
 
     def __repr__(self) -> str:
         return "<ChannelView from={} to={} capacity={}>".format(
