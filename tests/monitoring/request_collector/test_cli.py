93db2a886fc1b82c40d55a6224c5137908d4c8d2
Paul Lange <palango@gmx.de>
2019-06-11 15:55:52 +0200

    Review and furher fixes

--- tests/monitoring/request_collector/test_cli.py
+++ tests/monitoring/request_collector/test_cli.py
@@ -11,7 +11,9 @@ def test_success(default_cli_args):
     """ Calling the request_collector with default args should succeed after heavy mocking """
     runner = CliRunner()
     with patch.multiple(**PATCH_ARGS):
-        result = runner.invoke(main, default_cli_args, catch_exceptions=False)
+        result = runner.invoke(
+            main, default_cli_args + ["--chain-id", "mainnet"], catch_exceptions=False
+        )
     assert result.exit_code == 0
 
 
@@ -20,7 +22,9 @@ def test_wrong_password(default_cli_args):
     runner = CliRunner()
     with patch.multiple(**PATCH_ARGS):
         result = runner.invoke(
-            main, default_cli_args + ["--password", "wrong"], catch_exceptions=False
+            main,
+            default_cli_args + ["--chain-id", "mainnet", "--password", "wrong"],
+            catch_exceptions=False,
         )
     assert result.exit_code == 1
 
@@ -30,7 +34,9 @@ def test_shutdown(default_cli_args):
     runner = CliRunner()
     with patch.multiple(**PATCH_ARGS) as mocks:
         mocks["RequestCollector"].return_value.run.side_effect = KeyboardInterrupt
-        result = runner.invoke(main, default_cli_args, catch_exceptions=False)
+        result = runner.invoke(
+            main, default_cli_args + ["--chain-id", "mainnet"], catch_exceptions=False
+        )
         assert result.exit_code == 0
         assert "Exiting" in result.output
         assert mocks["RequestCollector"].return_value.listen_forever.called
@@ -42,7 +48,9 @@ def test_log_level(default_cli_args):
     with patch.multiple(**PATCH_ARGS), patch("logging.basicConfig") as basic_config:
         for log_level in ("CRITICAL", "WARNING"):
             runner.invoke(
-                main, default_cli_args + ["--log-level", log_level], catch_exceptions=False
+                main,
+                default_cli_args + ["--chain-id", "mainnet", "--log-level", log_level],
+                catch_exceptions=False,
             )
             # pytest already initializes logging, so basicConfig does not have
             # an effect. Use mocking to check that it's called properly.
