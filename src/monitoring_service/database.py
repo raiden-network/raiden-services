raiden-network/raiden-services

commit e9110e2cf5ccb91b2e3e84e99164104c03478fa5

Prevent acceptance of bad reward proof
By checking that both MR signatures are done by the same key, attackers
are prevented from adding a bad reward proof to an otherwise valid MR.
This prevents
https://github.com/raiden-network/raiden-services/issues/418
immediately. After
https://github.com/raiden-network/raiden-contracts/issues/1117 is done,
this will be a much more useful check because it will not only fail when
one of the proofs is tampered with, but also when they were not intended
to be used in the same MR.

Closes https://github.com/raiden-network/raiden-services/issues/418.

src/request_collector/server.py
@@ -84,6 +84,8 @@ class RequestCollector(gevent.Greenlet):
         if monitor_request.chain_id != self.chain_id:
             log.debug("Bad chain_id", monitor_request=monitor_request, expected=self.chain_id)
             return
+        if monitor_request.non_closing_signer != monitor_request.reward_proof_signer:
+            log.debug("The two MR signatures don't match", monitor_request=monitor_request)
 
         # Check that received MR is newer by comparing nonces
         old_mr = self.state_db.get_monitor_request(

commit fac691cd9028701375c16dc454290c3748373710

Fix recently introduced reward signature problems
Caused by e9110e2cf5ccb91b2e3e84e99164104c03478fa5 and
693c9701ed41b2a471df53af5dea311aee6335cd.

src/monitoring_service/database.py
@@ -64,9 +64,11 @@ class SharedDatabase(BaseDatabase):
     ) -> Optional[MonitorRequest]:
         row = self.conn.execute(
             """
-                SELECT *,
-                    (SELECT chain_id FROM blockchain) As chain_id
-                FROM monitor_request
+                SELECT monitor_request.*,
+                    blockchain.chain_id,
+                    blockchain.monitor_contract_address AS msc_address
+                FROM monitor_request,
+                    blockchain
                 WHERE channel_identifier = ?
                   AND token_network_address = ?
                   AND non_closing_signer = ?
@@ -82,6 +84,7 @@ class SharedDatabase(BaseDatabase):
 
         kwargs = {key: val for key, val in zip(row.keys(), row) if key != "non_closing_signer"}
         kwargs["token_network_address"] = to_canonical_address(kwargs["token_network_address"])
+        kwargs["msc_address"] = to_canonical_address(kwargs["msc_address"])
         kwargs["closing_signature"] = decode_hex(kwargs["closing_signature"])
         kwargs["non_closing_signature"] = decode_hex(kwargs["non_closing_signature"])
         kwargs["reward_proof_signature"] = decode_hex(kwargs["reward_proof_signature"])

src/monitoring_service/states.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Iterable, Optional
 
-from eth_utils import decode_hex, encode_hex
+from eth_utils import decode_hex, encode_hex, to_checksum_address
 from web3 import Web3
 
 from raiden.constants import EMPTY_SIGNATURE
@@ -20,6 +20,7 @@ from raiden.utils.typing import (
     TransactionHash,
 )
 from raiden_contracts.constants import ChannelState, MessageTypeId
+from raiden_contracts.utils.proofs import pack_reward_proof
 from raiden_libs.states import BlockchainState
 
 
@@ -139,7 +140,9 @@ class HashedBalanceProof:
         request_monitoring.sign(non_closing_signer)
         return request_monitoring
 
-    def get_monitor_request(self, privkey: str, reward_amount: TokenAmount) -> "MonitorRequest":
+    def get_monitor_request(
+        self, privkey: str, reward_amount: TokenAmount, msc_address: Address
+    ) -> "MonitorRequest":
         """Get monitor request message for a given balance proof."""
         return UnsignedMonitorRequest(
             channel_identifier=self.channel_identifier,
@@ -150,6 +153,7 @@ class HashedBalanceProof:
             additional_hash=self.additional_hash,
             closing_signature=self.signature,
             reward_amount=reward_amount,
+            msc_address=msc_address,
         ).sign(privkey)
 
 
@@ -172,6 +176,7 @@ class UnsignedMonitorRequest:
     closing_signature: Signature
 
     # reward info
+    msc_address: Address
     reward_amount: TokenAmount
 
     # extracted from signature
@@ -184,7 +189,7 @@ class UnsignedMonitorRequest:
 
     @classmethod
     def from_balance_proof(
-        cls, balance_proof: HashedBalanceProof, reward_amount: TokenAmount
+        cls, balance_proof: HashedBalanceProof, reward_amount: TokenAmount, msc_address: Address
     ) -> "UnsignedMonitorRequest":
         return cls(
             channel_identifier=balance_proof.channel_identifier,
@@ -195,10 +200,12 @@ class UnsignedMonitorRequest:
             additional_hash=balance_proof.additional_hash,
             closing_signature=balance_proof.signature,
             reward_amount=reward_amount,
+            msc_address=msc_address,
         )
 
     def sign(self, priv_key: str) -> "MonitorRequest":
         local_signer = LocalSigner(private_key=decode_hex(priv_key))
+        non_closing_signature = local_signer.sign(self.packed_non_closing_data())
         return MonitorRequest(
             channel_identifier=self.channel_identifier,
             token_network_address=self.token_network_address,
@@ -207,9 +214,12 @@ class UnsignedMonitorRequest:
             nonce=self.nonce,
             additional_hash=self.additional_hash,
             closing_signature=self.closing_signature,
+            non_closing_signature=non_closing_signature,
             reward_amount=self.reward_amount,
-            reward_proof_signature=local_signer.sign(self.packed_reward_proof_data()),
-            non_closing_signature=local_signer.sign(self.packed_non_closing_data()),
+            reward_proof_signature=local_signer.sign(
+                self.packed_reward_proof_data(non_closing_signature)
+            ),
+            msc_address=self.msc_address,
         )
 
     def packed_balance_proof_data(
@@ -225,14 +235,13 @@ class UnsignedMonitorRequest:
             (decode_hex(self.additional_hash), "bytes32"),
         )
 
-    def packed_reward_proof_data(self) -> bytes:
+    def packed_reward_proof_data(self, non_closing_signature: Signature) -> bytes:
         """Return reward proof data serialized to binary"""
-        return pack_data(
-            (self.channel_identifier, "uint256"),
-            (self.reward_amount, "uint256"),
-            (self.token_network_address, "address"),
-            (self.chain_id, "uint256"),
-            (self.nonce, "uint256"),
+        return pack_reward_proof(
+            monitoring_service_contract_address=to_checksum_address(self.msc_address),
+            chain_id=self.chain_id,
+            non_closing_signature=non_closing_signature,
+            reward_amount=self.reward_amount,
         )
 
     def packed_non_closing_data(self) -> bytes:
@@ -259,5 +268,6 @@ class MonitorRequest(UnsignedMonitorRequest):
             data=self.packed_non_closing_data(), signature=self.non_closing_signature
         )
         self.reward_proof_signer = recover(
-            data=self.packed_reward_proof_data(), signature=self.reward_proof_signature
+            data=self.packed_reward_proof_data(self.non_closing_signature),
+            signature=self.reward_proof_signature,
         )

src/request_collector/server.py
@@ -75,6 +75,7 @@ class RequestCollector(gevent.Greenlet):
                 reward_amount=request_monitoring.reward_amount,
                 # FIXME: not sure why the Signature call is necessary
                 reward_proof_signature=Signature(request_monitoring.signature),
+                msc_address=request_monitoring.monitoring_service_contract_address,
             )
         except InvalidSignature:
             log.info("Ignore MR with invalid signature", monitor_request=request_monitoring)

tests/constants.py
@@ -1,2 +1,5 @@
+from raiden.utils.typing import Address
+
 KEYSTORE_FILE_NAME = "keystore.txt"
 KEYSTORE_PASSWORD = "password"
+TEST_MSC_ADDRESS = Address(b"9" * 20)

tests/monitoring/fixtures/server.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 import pytest
 from eth_utils import decode_hex
 from request_collector.server import RequestCollector
+from tests.constants import TEST_MSC_ADDRESS
 from web3 import Web3
 
 from monitoring_service.database import Database
@@ -44,7 +45,7 @@ def ms_database():
     return Database(
         filename=":memory:",
         chain_id=ChainID(1),
-        msc_address=Address(bytes([2] * 20)),
+        msc_address=TEST_MSC_ADDRESS,
         registry_address=Address(bytes([3] * 20)),
         receiver=Address(bytes([4] * 20)),
     )

tests/monitoring/monitoring_service/factories.py
@@ -1,5 +1,7 @@
 import random
 
+from tests.constants import TEST_MSC_ADDRESS
+
 from monitoring_service.states import (
     Channel,
     HashedBalanceProof,
@@ -46,7 +48,7 @@ def create_signed_monitor_request(
         priv_key=closing_privkey,
     )
     monitor_request = UnsignedMonitorRequest.from_balance_proof(
-        bp, reward_amount=reward_amount
+        bp, reward_amount=reward_amount, msc_address=TEST_MSC_ADDRESS
     ).sign(nonclosing_privkey)
 
     # Some signature correctness checks

tests/monitoring/monitoring_service/test_crash.py
@@ -3,6 +3,7 @@ from typing import List
 from unittest.mock import Mock
 
 from eth_utils import decode_hex, encode_hex
+from tests.constants import TEST_MSC_ADDRESS
 
 from monitoring_service.events import ActionMonitoringTriggeredEvent
 from monitoring_service.service import MonitoringService
@@ -50,7 +51,7 @@ def test_crash(
         locksroot=encode_hex(LOCKSROOT_OF_NO_LOCKS),
     )
     monitor_request = balance_proof.get_monitor_request(
-        get_private_key(c2), reward_amount=TokenAmount(0)
+        get_private_key(c2), reward_amount=TokenAmount(0), msc_address=TEST_MSC_ADDRESS
     )
 
     events = [

tests/monitoring/monitoring_service/test_database.py
@@ -93,7 +93,7 @@ def test_save_and_load_monitor_request(ms_database: Database):
         non_closing_signer=request.non_closing_signer,
     )
 
-    assert request == restored
+    assert restored == request
 
 
 def test_save_and_load_channel(ms_database: Database):