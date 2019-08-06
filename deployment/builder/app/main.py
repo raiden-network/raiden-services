47e69066e04ef0fdcaccf41fa2166a534a913aef
Paul Lange <palango@gmx.de>
2019-08-06 11:21:12 +0200

    Update `builder` to work on docker hub webhook

--- deployment/builder/app/main.py
+++ deployment/builder/app/main.py
@@ -1,37 +1,40 @@
+import json
 import os
 import subprocess
 import sys
 from pprint import pprint
+from typing import List
 
+import requests
 from flask import Flask, request
 
+ALL_SERVICE_NAMES = [
+    "pfs-ropsten",
+    "pfs-rinkeby",
+    "pfs-kovan",
+    "pfs-goerli",
+    "pfs-ropsten-with-fee",
+    "pfs-rinkeby-with-fee",
+    "pfs-kovan-with-fee",
+    "pfs-goerli-with-fee",
+    "ms-ropsten",
+    "ms-rinkeby",
+    "ms-kovan",
+    "ms-goerli",
+    "ms-goerli-backup",
+    "msrc-ropsten",
+    "msrc-rinkeby",
+    "msrc-kovan",
+    "msrc-goerli",
+    "msrc-goerli-backup",
+]
 
 REPOS = {
-    "raiden-network/raiden-services": {
+    "latest": {
         "master": {
             "source": "/root/raiden-services",
             "deployment": "/root/raiden-services/deployment",
-        },
-        "names": [
-            "pfs-ropsten",
-            "pfs-rinkeby",
-            "pfs-kovan",
-            "pfs-goerli",
-            "pfs-ropsten-with-fee",
-            "pfs-rinkeby-with-fee",
-            "pfs-kovan-with-fee",
-            "pfs-goerli-with-fee",
-            "ms-ropsten",
-            "ms-rinkeby",
-            "ms-kovan",
-            "ms-goerli",
-            "ms-goerli-backup",
-            "msrc-ropsten",
-            "msrc-rinkeby",
-            "msrc-kovan",
-            "msrc-goerli",
-            "msrc-goerli-backup",
-        ],
+        }
     }
 }
 
@@ -49,33 +52,50 @@ def print_to_stderr(s):
 @app.route("/", methods=["get", "post"])
 def main():
     data = request.json or {}
-    repo = data.get("repository", {}).get("full_name", "")
-    branch = data.get("ref", "").replace("refs/heads/", "")
-    branch_config = REPOS.get(repo)
-    if branch_config and branch in branch_config:
+    print_to_stderr("Received request at '/'. Ignoring!")
+    print_to_stderr(json.dumps(data))
+
+    return "OK"
+
+
+# See https://docs.docker.com/docker-hub/webhooks/
+@app.route("/dockerhub", methods=["get", "post"])
+def dockerhub():
+    data = request.json or {}
+    print_to_stderr("Received request at '/dockerhub'.")
+    print_to_stderr(json.dumps(data))
+
+    callback_url = data.get("callback_url")
+    push_data = data.get("push_data", {})
+    tag = push_data.get("tag")
+
+    deploy_config = REPOS.get(tag)
+
+    if deploy_config:
         try:
-            res = update(branch, branch_config["names"], **branch_config[branch])
+            update(ALL_SERVICE_NAMES, **deploy_config["master"])
+
+            if callback_url:
+                requests.post(url=callback_url, json={"state": "success"})
+
+            if callback_url:
+                requests.post(url=callback_url, json={"state": "success"})
+            return "OK"
+
         except ImageUpdateError:
             print_to_stderr("Error updating local images via docker registry!")
         except Exception as e:
-            print_to_stderr(f"Fatal Error while updating images: Unhandled exception encountered: {e}")
-            raise
-        else:
-            pprint(
-                {
-                    "repo": repo,
-                    "branch": branch,
-                    "head_commit": data["head_commit"],
-                    "pusher": data["pusher"],
-                    "build_result": res,
-                },
-                stream=sys.stderr,
+            print_to_stderr(
+                f"Fatal Error while updating images: Unhandled exception encountered: {e}"
             )
+            raise
         finally:
-            return "OK"
+            if callback_url:
+                requests.post(url=callback_url, json={"state": "error"})
+    return "OK"
 
 
-def update(branch, container_names, source, deployment, **kw):
+def update(container_names: List[str], source: str, deployment: str) -> None:
     print_to_stderr(f"Changing working directory to {source}")
     try:
         os.chdir(source)
@@ -92,7 +112,7 @@ def update(branch, container_names, source, deployment, **kw):
 
     print_to_stderr("Resetting branch: git reset")
     try:
-        subprocess.check_output(["git", "reset", "--hard", f"origin/{branch}"])
+        subprocess.check_output(["git", "reset", "--hard", "origin/master"])
     except subprocess.SubprocessError as e:
         print_to_stderr(f"Resetting branch failed: {e}")
         raise ImageUpdateError from e
