0399a043962e30f03bbcd1135765bf6251444bc1
Paul Lange <palango@gmx.de>
2019-08-08 11:54:05 +0200

    PR review

--- tests/monitoring/monitoring_service/test_service.py
+++ tests/monitoring/monitoring_service/test_service.py
@@ -12,14 +12,11 @@ def test_check_pending_transactions(web3, wait_for_blocks, monitoring_service: M
 
     for tx_status in (0, 1):
         tx_receipt = {"blockNumber": web3.eth.blockNumber, "status": tx_status}
-        with patch.object(web3.eth, "getTransactionReceipt", Mock(return_value=tx_receipt)):
-            with patch.object(
-                monitoring_service.database, "remove_waiting_transaction"
-            ) as remove_mock:
+        with patch.object(
+            web3.eth, "getTransactionReceipt", Mock(return_value=tx_receipt)
+        ), patch.object(monitoring_service.database, "remove_waiting_transaction") as remove_mock:
+            for should_call in (False, False, False, True):
+                monitoring_service._check_pending_transactions()  # pylint: disable=protected-access # noqa
 
-                for should_call in (False, False, False, True):
-                    monitoring_service.context.last_known_block = web3.eth.blockNumber
-                    monitoring_service._check_pending_transactions()  # pylint: disable=protected-access # noqa
-
-                    assert remove_mock.called == should_call
-                    wait_for_blocks(1)
+                assert remove_mock.called == should_call
+                wait_for_blocks(1)
