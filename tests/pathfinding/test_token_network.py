23b1eab52cab305a0dc962e6bc284031256c2e15
Paul Lange <palango@gmx.de>
2019-08-21 14:52:28 +0200

    Add test for `prune_graph`

--- tests/pathfinding/test_token_network.py
+++ tests/pathfinding/test_token_network.py
@@ -1,6 +1,11 @@
 from typing import List
 
+from networkx import DiGraph
+
 from pathfinding_service.model import TokenNetwork
+from pathfinding_service.model.token_network import prune_graph
+from raiden.network.transport.matrix import AddressReachability
+from raiden.tests.utils.factories import make_address
 from raiden.utils.typing import Address, ChannelID
 
 
@@ -49,3 +54,31 @@ def test_tn_multiple_channels_for_two_participants_opened(
 
     # there should be one channel left
     assert len(token_network_model.channel_id_to_addresses) == 1
+
+
+def test_graph_pruning():
+    participant1 = make_address()
+    participant2 = make_address()
+    participant3 = make_address()
+
+    graph = DiGraph()
+    graph.add_edge(participant1, participant2, view=12)
+    graph.add_edge(participant2, participant1, view=21)
+    graph.add_edge(participant2, participant3, view=23)
+    graph.add_edge(participant3, participant2, view=32)
+
+    all_reachable = {
+        p: AddressReachability.REACHABLE for p in (participant1, participant2, participant3)
+    }
+    pruned_all_reachable = prune_graph(graph=graph, address_to_reachability=all_reachable)
+    assert len(pruned_all_reachable.edges) == len(graph.edges)
+
+    p1_not_reachable = all_reachable.copy()
+    p1_not_reachable[participant1] = AddressReachability.UNREACHABLE
+    pruned_p1_unreachbale = prune_graph(graph=graph, address_to_reachability=p1_not_reachable)
+    assert len(pruned_p1_unreachbale.edges) == 2  # just the two edges between 2 and 3 left
+
+    p2_not_reachable = all_reachable.copy()
+    p2_not_reachable[participant2] = AddressReachability.UNREACHABLE
+    pruned_p2_unreachbale = prune_graph(graph=graph, address_to_reachability=p2_not_reachable)
+    assert len(pruned_p2_unreachbale.edges) == 0  # 2 is part of all channels
