bd441b146d73a73220965280ad056c10cf8e4e64
Karl Bartel <karl42@gmail.com>
2020-04-27 13:04:29 +0200

    Change MS DEFAULT_MIN_REWARD to Raiden's default
    We don't have a communication channel to establish a good reward during
    runtime, so using the same fixed value is best in the meantime.
    This also prevents MS operators from accidentally not demanding a
    reward, which is a bad idea since they have to pay transaction fees.

--- src/monitoring_service/constants.py
+++ src/monitoring_service/constants.py
@@ -6,6 +6,9 @@ MAX_FILTER_INTERVAL: int = 100_000
 DEFAULT_GAS_BUFFER_FACTOR: int = 10
 DEFAULT_GAS_CHECK_BLOCKS: int = 100
 KEEP_MRS_WITHOUT_CHANNEL: timedelta = timedelta(minutes=15)
+# Make sure this stays <= Raiden's MONITORING_REWARD until there is a way to
+# inform Raiden about the expected rewards.
+DEFAULT_MIN_REWARD = 5 * 10 ** 18
 
 # A LockedTransfer message is roughly 1kb. Having 1000/min = 17/sec will be
 # hard to achieve outside of benchmarks for now. To have some safety margin for
