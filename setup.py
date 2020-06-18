00afafe260d451765d36f7f71759f80aee1e2bf2
Paul Lange <palango@gmx.de>
2020-06-18 11:24:35 +0200

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="raiden-services",
-    version="0.10.0",
+    version="0.11.0",
     license="MIT",
     description=DESCRIPTION,
     long_description=README,
