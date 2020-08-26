b3524e8243586e39e87e78807060d94705d3b12e
Maximilian <maxdacjs@gmail.com>
2020-08-26 17:58:19 +0200

    Change testing setup for metrics and get rid of teardown
    The current teardown mechanism for the global state of the prometheus metrics
    do not work in conjunction with metrics that query dynamically bound callback methods.
    Since modifying the teardown mechanism would get very involved with replicating behaviour
    from the prometheus_client library, the decision was made to accept global state in the tests
    and only test for deltas on the metrics. In every test where a change of value in a metric
    has to be asserted, the state of the metrics registry has to be saved before initiating
    the action that will trigger the metric's value change.
    
    Additionally, some convenience functionality is introduced, allowing for
    easier retrieval of metrics objects, as well as label-dictionary construction.

--- src/monitoring_service/metrics.py
+++ src/monitoring_service/metrics.py
@@ -1,23 +1,22 @@
-from enum import Enum, unique
-
 from prometheus_client import CollectorRegistry, Counter
 
+from raiden.utils.typing import TokenAmount
 from raiden_libs.metrics import (
-    REGISTRY,
     ERRORS_LOGGED,
-    EVENTS_PROCESSING_TIME,
     EVENTS_EXCEPTIONS_RAISED,
-    MESSAGES_PROCESSING_TIME,
+    EVENTS_PROCESSING_TIME,
     MESSAGES_EXCEPTIONS_RAISED,
-    LabelErrorCategory,
+    MESSAGES_PROCESSING_TIME,
+    REGISTRY,
+    ErrorCategory,
+    MetricsEnum,
     collect_event_metrics,
-    collect_message_metrics
+    collect_message_metrics,
+    get_metrics_for_label,
 )
 
 
-
-@unique
-class LabelWho(Enum):
+class Who(MetricsEnum):
     US = "us"
     THEY = "they"
 
@@ -25,14 +24,18 @@ class LabelWho(Enum):
 REWARD_CLAIMS = Counter(
     "economics_reward_claims_successful_total",
     "The number of overall successful reward claims",
-    labelnames=["who"],
+    labelnames=[Who.label_name()],
     registry=REGISTRY,
 )
 
 REWARD_CLAIMS_TOKEN = Counter(
     "economics_reward_claims_token_total",
     "The amount of token earned by reward claims",
-    labelnames=["who"],
+    labelnames=[Who.label_name()],
     registry=REGISTRY,
 )
 
+
+def report_increased_reward_claims(amount: TokenAmount, who: Who) -> None:
+    get_metrics_for_label(REWARD_CLAIMS, who).inc()
+    get_metrics_for_label(REWARD_CLAIMS_TOKEN, who).inc(float(amount))
--- tests/libs/fixtures/__init__.py
+++ tests/libs/fixtures/__init__.py
@@ -1,7 +1,6 @@
 from .cli import default_cli_args
 from .network import free_port
 from .web3 import contracts_manager, keystore_file, mockchain, wait_for_blocks
-from .metrics import prometheus_client_teardown, prometheus_client_collectors
 
 # Without declaring `__all__`, the names `cli` and `web3` would get imported
 # when doing `from libs.fixtures import *`, which is not intended.
@@ -12,6 +11,4 @@ __all__ = [
     "mockchain",
     "default_cli_args",
     "free_port",
-    "prometheus_client_collectors",
-    "prometheus_client_teardown"
 ]
