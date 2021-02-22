d8c5f9a2f803751485c8dc1f0f8d600ea00ae04d
Karl Bartel <karl42@gmail.com>
2021-02-22 16:09:04 +0100

    Include matrix user ids in route response
    I'm using the protected `_address_to_userids` to make this work. It
    would make sense to provide a public way to get this information during
    a future refactoring.
    
    Closes https://github.com/raiden-network/raiden-services/issues/928

--- tests/pathfinding/utils.py
+++ tests/pathfinding/utils.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from unittest import mock
+
+from eth_utils import to_checksum_address
 
 from raiden.network.transport.matrix import AddressReachability
 from raiden.network.transport.matrix.utils import ReachabilityState
@@ -10,6 +13,10 @@ class SimpleReachabilityContainer:  # pylint: disable=too-few-public-methods
         self.reachabilities = reachabilities
         self.times = {address: datetime.utcnow() for address in reachabilities}
         self._userid_to_presence: dict = {}
+        self._address_to_userids: dict = mock.MagicMock()
+        self._address_to_userids.__getitem__ = (
+            lambda self, key: f"{to_checksum_address(key)}@homeserver"
+        )
 
     def get_address_reachability(self, address: Address) -> AddressReachability:
         return self.reachabilities.get(address, AddressReachability.UNKNOWN)
