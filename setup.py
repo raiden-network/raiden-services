304a5092fbcf1571edc42861753d16ce80bddad8
Karl Bartel <karl42@gmail.com>
2021-04-09 14:13:15 +0200

    Bump version: 0.14.1 → 0.14.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="raiden-services",
-    version="0.14.1",
+    version="0.14.2",
     license="MIT",
     description=DESCRIPTION,
     long_description=README,
