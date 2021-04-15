fde698e3c8c21292bd95ac49ac3da87313fe9744
Max Langenfeld <max@brainbot.com>
2021-04-15 15:26:36 +0200

    Bump version: 0.14.2 → 0.14.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="raiden-services",
-    version="0.14.2",
+    version="0.14.3",
     license="MIT",
     description=DESCRIPTION,
     long_description=README,
