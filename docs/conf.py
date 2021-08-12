b372c22367795755e8c3da3da232f4a9e9f0ee89
Paul Lange <palango@gmx.de>
2021-08-12 16:36:12 +0200

    Bump version: 0.15.4 → 0.16.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Brainbot Labs Est.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.15.4'
+release = '0.16.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="raiden-services",
-    version="0.15.4",
+    version="0.16.0",
     license="MIT",
     description=DESCRIPTION,
     long_description=README,
