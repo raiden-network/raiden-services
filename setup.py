85bc6c5a5f9ecfa327c13a765a1a70928190946e
Frederik Lührs <luehrs.fred@gmail.com>
2021-10-01 12:44:28 +0200

    Bump version: 0.18.1 → 0.18.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 setup(
     name="raiden-services",
-    version="0.18.1",
+    version="0.18.2",
     license="MIT",
     description=DESCRIPTION,
     long_description=README,
