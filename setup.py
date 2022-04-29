40c765cec9f99c4ff9a6164ddb224b77cb60b737
Thore Strassburg <thore@weilbier.net>
2022-04-29 17:53:48 +0200

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 setup(
     name="raiden-services",
-    version="2.0.0",
+    version="2.0.1",
     license="MIT",
     description=DESCRIPTION,
     long_description=README,
